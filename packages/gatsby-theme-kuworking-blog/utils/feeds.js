6841f093da63f7255a8a44ce24a770f88de241fa
kuworking <kuworking@gmail.com>
2019-11-06 10:28:19 +0100

    fix

--- packages/gatsby-theme-kuworking-blog/utils/feeds.js
+++ packages/gatsby-theme-kuworking-blog/utils/feeds.js
@@ -17,8 +17,6 @@ module.exports = site => {
           enclosure: {
             type: 'image/jpeg',
             url: site.siteUrl + get_image(post_images.edges, get_last_slug(edge.node.slug)).node.publicURL,
-        },
-
           },
           // custom_elements: [{ 'content:encoded': edge.node.html }],
         })
