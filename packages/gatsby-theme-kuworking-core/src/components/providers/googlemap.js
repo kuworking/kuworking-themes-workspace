18126a1f5ae8a25ee0768d5a1013040747b41708
kuworking <kuworking@gmail.com>
2019-11-16 14:05:35 +0100

    lazy youtube implemented

--- packages/gatsby-theme-kuworking-core/src/components/providers/googlemap.js
+++ packages/gatsby-theme-kuworking-core/src/components/providers/googlemap.js
@@ -1,6 +1,16 @@
+import React from 'react'
 import styled from '@emotion/styled'
 
-export const GoogleMap = ({ src, component }) => <Frame component={component} title="Google Maps" src={src} />
+export const GoogleMap = ({ src, component }) => (
+  <Frame
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+    frameborder="0"
+    allowfullscreen
+    component={component}
+    title="Google Maps"
+    src={src}
+  />
+)
 
 const Frame = styled.iframe`
   width: 100%;
