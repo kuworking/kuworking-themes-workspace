21dc66645926812bca245272d747872d8ec3fffc
kuworking <kuworking@gmail.com>
2019-12-17 21:43:28 +0100

    pinterestscript embedded in pinterest component

--- packages/gatsby-theme-kuworking-core/src/components/providers/pinterest.js
+++ packages/gatsby-theme-kuworking-core/src/components/providers/pinterest.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { useScript } from '../hooks/usescript'
 
-export const Pinterest = ({ src }) => (
-  <a aria-label="Pinterest" data-pin-do="embedPin" data-pin-width="large" data-pin-build="doBuild" href={src}></a>
-)
+export const Pinterest = ({ src }) => {
+  const [loaded, error] = useScript('//assets.pinterest.com/js/pinit.js')
+
+  return (
+    <>
+      <a aria-label="Pinterest" data-pin-do="embedPin" data-pin-width="large" data-pin-build="doBuild" href={src}></a>
+      {loaded && window.doBuild && window.doBuild()}
+    </>
+  )
+}
 
 export const PinterestScript = () => {
   const [loaded, error] = useScript('//assets.pinterest.com/js/pinit.js')
