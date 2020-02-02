c6fa9643b127acc89b34661edc596ea585bf75a7
kuworking <kuworking@gmail.com>
2020-02-02 09:33:38 +0100

    fix config files

--- packages/gatsby-theme-kuworking-affiliate/utils/default-options.js
+++ packages/gatsby-theme-kuworking-affiliate/utils/default-options.js
@@ -1,16 +1,14 @@
 const info = require(`./info`)
-const buildManifest = require(`./manifest`)
 
 module.exports = themeOptions => {
   const source = themeOptions.info || info
 
   return {
     standalone: source.standalone || true,
-    jsonPath: source.website.jsonPath || 'content/json',
-    basePath: source.website.basePath || `/`,
-    folders_to_check: source.folders_to_check || ['content/json'],
-
-    manifest: buildManifest(source.metaData),
+    basePath: source.basePath || `/`,
+    jsonPath: source.jsonPath || 'content/json',
+    folders_to_check: [source.jsonPath] || ['content/json'],
+    manifest: source.manifest,
     tagmanager: source.tagManager || '',
     sitemap: source.siteMapExclude || [],
   }
--- packages/gatsby-theme-kuworking-affiliate/utils/info.js
+++ packages/gatsby-theme-kuworking-affiliate/utils/info.js
@@ -1,15 +1,29 @@
+const standalone = true
+const basePath = '/'
+const jsonPath = '/content/json'
+const site = 'kuworking'
+
 module.exports = {
-  standalone: true,
-  website: {
-    basePath: '/',
-  },
+  standalone: standalone,
+  basePath: basePath,
+  jsonPath: jsonPath,
+  folders_to_check: [jsonPath],
   metaData: {
-    site: 'kuworking',
+    site: site,
     title: 'Gatsby Theme kuworking affiliate',
     description: 'Gatsby Theme kuworking affiliate',
     siteUrl: 'https://www.kuworking.com',
   },
   tagManager: '',
   siteMapExclude: ['/dummy'],
-  folders_to_check: ['content/json'],
+  manifest: {
+    name: site,
+    short_name: site,
+    start_url: basePath || '/',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    display: `minimal-ui`,
+    icon: `content/icons/favicon.png`,
+    include_favicon: true,
+  },
 }
