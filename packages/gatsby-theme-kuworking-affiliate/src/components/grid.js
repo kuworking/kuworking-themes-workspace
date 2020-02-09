2d1f63e4b0c87f49875b858866953e5f0f4ce2d5
kuworking <kuworking@gmail.com>
2020-02-09 22:50:57 +0100

    simplify

--- packages/gatsby-theme-kuworking-affiliate/src/components/grid.js
+++ packages/gatsby-theme-kuworking-affiliate/src/components/grid.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import { CtaMain, GridItems, useGridMasonry } from 'gatsby-theme-kuworking-affiliate'
+import styled from '@emotion/styled'
+import { Styled } from 'theme-ui'
+import { CtaMain, GridCard, useGridMasonry } from 'gatsby-theme-kuworking-affiliate'
 
 export const Grid = ({ blogGrid: { core, posts }, shape }) => {
   const row_unit = 20
   const grid_gap = 15
-  const [repaint, setRepaint] = useState(true) // to trigger repaint
-  const [gridAutoRows, assignRef, updateGrid] = useGridMasonry(row_unit, grid_gap, [repaint, setRepaint])
+  const [gridAutoRows, assignRef, updateGrid] = useGridMasonry(row_unit, grid_gap)
 
   const data = {}
   core.forEach(({ node: { content } }) => {
@@ -18,7 +19,46 @@ export const Grid = ({ blogGrid: { core, posts }, shape }) => {
     <>
       <CtaMain />
 
-      <GridItems pieces={{ grid_gap, gridAutoRows, data, assignRef, updateGrid, shape }} />
+      <Title>BAGS</Title>
+      <Container row_unit={gridAutoRows} grid_gap={grid_gap}>
+        {data.bags.map((item, i) => (
+          <GridCard
+            key={'bags' + i}
+            item={item}
+            category="bags"
+            adjustMasonry={updateGrid}
+            ref={assignRef}
+            shape={shape}
+          />
+        ))}
+      </Container>
+
+      <Title>TECH</Title>
+      <Container row_unit={gridAutoRows} grid_gap={grid_gap}>
+        {data.tech.map((item, i) => (
+          <GridCard
+            key={'tech' + i}
+            item={item}
+            category="tech"
+            adjustMasonry={updateGrid}
+            ref={assignRef}
+            shape={shape}
+          />
+        ))}
+      </Container>
     </>
   )
 }
+
+const Container = styled.div`
+  width: 100%;
+  transition: all 0.5s ease;
+
+  display: grid;
+  grid-column-gap: 10px;
+  grid-row-gap: ${props => props.grid_gap}px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: ${props => props.row_unit}px;
+`
+
+const Title = styled(Styled.h1)``
