8885eeed1311c596fcd81f69919f0ceb6ddd3a35
kuworking <kuworking@gmail.com>
2020-04-04 21:09:30 +0200

    babel fix

--- packages/gatsby-theme-kuworking-landing-one/src/components/header.js
+++ packages/gatsby-theme-kuworking-landing-one/src/components/header.js
@@ -5,10 +5,7 @@ import { Sunny as Day } from 'emotion-icons/ion-md'
 import { Moon as Night } from 'emotion-icons/fa-regular'
 import { useColorMode } from 'theme-ui'
 import { modes } from '../gatsby-plugin-theme-ui/index'
-import {
-  text,
-  // Switch
-} from 'gatsby-theme-kuworking-landing-one'
+import { text, Switch } from 'gatsby-theme-kuworking-landing-one'
 
 export const Header = ({ basePath, ShapeButton, typeOfGridButton }) => (
   <Parent>
@@ -23,7 +20,6 @@ export const Header = ({ basePath, ShapeButton, typeOfGridButton }) => (
       {typeOfGridButton}
       {ShapeButton}
 
-      {/*
       <Switch
         Day={Day}
         Night={Night}
@@ -38,7 +34,6 @@ export const Header = ({ basePath, ShapeButton, typeOfGridButton }) => (
           nightColor: '#5c40719c',
         }}
       />
-      */}
     </header>
   </Parent>
 )
--- packages/test/src/index.js
+++
@@ -1,3 +0,0 @@
-export { Spinner01 } from './elements/spinner'
-export { shuffle_array } from './methods/methods'
-export { Instagram } from './providers/instagram'
