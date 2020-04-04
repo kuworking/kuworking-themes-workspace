7f8465ac8ae28265f6621996a7b2cd89064ceaef
kuworking <kuworking@gmail.com>
2020-04-04 09:52:07 +0200

    test workspace

---
+++ packages/test/src/elements/spinner.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export const Spinner01 = () => (
+  <Spinning>
+    <div></div>
+    <div></div>
+    <div></div>
+  </Spinning>
+)
+
+const spinner = '10px'
+const orbit = '1'
+const angle = 320 * (Math.PI / 180)
+const border = '0px'
+const delay = '-0.04s'
+const Spinning = styled.div`
+  display: inline-block;
+  position: relative;
+  width: ${spinner};
+  height: ${spinner};
+  border: ${border} solid #ff6c00;
+  border-radius: 50%;
+  margin-right: 10px;
+
+  & div {
+    animation: lds-roller 2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    transform-origin: calc(${spinner} / 2) calc(${spinner} / 2);
+    margin-left: -${border};
+    margin-top: -${border};
+  }
+
+  & div:after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+    background-color: #ff6c00;
+  }
+
+  & div:nth-of-type(1) {
+    animation-delay: calc(1 * ${delay});
+  }
+  & div:nth-of-type(2) {
+    animation-delay: calc(5 * ${delay});
+  }
+  & div:nth-of-type(3) {
+    animation-delay: calc(10 * ${delay});
+  }
+  & div:nth-of-type(n):after {
+    top: ${orbit * Math.sin(angle)}px;
+    left: ${orbit * Math.cos(angle)}px;
+  }
+
+  @keyframes lds-roller {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`
