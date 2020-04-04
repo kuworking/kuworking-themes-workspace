a441a5996ede88fe96087d699c3766841bf4fc33
kuworking <kuworking@gmail.com>
2020-04-04 18:52:41 +0200

    back to 1.5, simplified

--- packages/kuworking-core/src/index.js
+++ packages/kuworking-core/src/index.js
@@ -1,44 +1,13 @@
-export { Fade } from './animations/fade'
+import { useRef } from 'react'
+import styled from '@emotion/styled'
 
-/*
-export { SEO } from './gatsby/seo'
-export { Switch } from './elements/switch'
-export { Spinner01 } from './elements/spinner'
-export { SocialShare } from './elements/socialshare'
+export const Test = () => {
+  const dumb = useRef()
+  return <Div>hola</Div>
+}
 
-export { Box } from './animations/box'
-export { Fade } from './animations/fade'
-export { Toggle } from './animations/toggle'
-export { Move } from './animations/move'
-export { LetterFade } from './animations/letterfade'
-export { LetterFade2 } from './animations/letterfade2'
-export { Zoom } from './animations/zoom'
-export { ZoomPanic } from './animations/zoompanic'
-export { Show } from './animations/show'
-export { Peek } from './animations/peek'
-export { Shadow } from './animations/shadow'
-
-export { domprogress } from './hooks/domprogress'
-export { useCss } from './hooks/usecss'
-export { useFlexMasonry } from './hooks/useflexmasonry'
-export { useGridMasonry } from './hooks/usegridmasonry'
-export { useLocalStorage } from './hooks/uselocalstorage'
-export { useScript } from './hooks/usescript'
-export { useWindowResize } from './hooks/usewindowresize'
-
-export { Disqus } from './disqus/disqus'
-
-export { CodeLink, useCodePenScript, CodePen } from './methods/codepen'
-export { Highlight, apply_highlight } from './methods/highlight'
-export { BImg, Img } from './methods/image'
-export { fixDate, shuffle_array, is_this_a_mobile_device, wait, get_response, get_request } from './methods/methods'
-export { Prism } from './methods/prism'
-
-export { GoogleMap } from './providers/googlemap'
-export { Instagram } from './providers/instagram'
-export { Pinterest, PinterestScript } from './providers/pinterest'
-export { Video } from './providers/video'
-export { Youtube } from './providers/youtube'
-
-
-*/
+const Div = styled.div`
+  padding: 10px;
+  background: #ccc;
+  border-radius: 10px;
+`
