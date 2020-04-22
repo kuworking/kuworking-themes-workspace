4b47dc30a70beef627c9c8218ab482b86ebe828f
kuworking <kuworking@gmail.com>
2020-04-22 17:53:51 +0200

    remove pagination

--- packages/gatsby-theme-kuworking-core/index.js
+++ packages/gatsby-theme-kuworking-core/index.js
@@ -1,4 +1,3 @@
-export { Pagination } from './src/components/pagination'
 export { config, text, seoText } from './src/utils/config'
 export { Page } from './src/templates/page-template'
 export { build_post_structure, structure_image_versions } from './src/gatsby-node/methodses6'
