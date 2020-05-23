70ea4ce54598e59c52ae208c759440bc0c61de91
kuworking <kuworking@gmail.com>
2020-05-23 16:26:22 +0200

    refactor

--- packages/gatsby-theme-kuworking-core/index.js
+++ packages/gatsby-theme-kuworking-core/index.js
@@ -1,3 +1,3 @@
-export { config, text, seoText } from './src/utils/config'
+export { config, seoText } from './src/utils/config'
 export { Page } from './src/templates/page-template'
 export { build_post_structure, structure_image_versions } from './src/gatsby-node/methodses6'
