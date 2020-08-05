ef5a18daa5eaf250cbed3b2409fa47b458ce0b03
kuworking <kuworking@gmail.com>
2020-08-05 12:16:05 +0200

    simplify structure

--- packages/gatsby-theme-kuworking-core/src/templates/posts-template.js
+++ packages/gatsby-theme-kuworking-core/src/templates/posts-template.js
@@ -2,43 +2,19 @@ import React from 'react'
 import { StructureGrid } from '../components/structure-grid'
 
 export default ({ pageContext, data = null }) => {
-  const {
-    pre_path,
-    basePath,
-    thePath,
-    types,
-    num_of_pages,
-    current_page,
-    posts,
-    wallpapers, // eslint-disable-line no-unused-vars
-    post_images,
-  } = pageContext
-
   const { tag, global_tags, this_is_a_tag_search } = pageContext
 
   return (
     <StructureGrid
       type="grid"
       blogGrid={{
-        posts,
-        query: data,
-        types,
-        thePath,
-        pagination: {
-          basePath,
-          pre_path,
-          num_of_pages,
-          current_page,
-          isFirst: current_page === 1,
-          isLast: current_page === num_of_pages,
-          prev_page: current_page - 1 === 1 ? `${pre_path}/` : `${pre_path}/` + (current_page - 1).toString(),
-          next_page: `${pre_path}/` + (current_page + 1).toString(),
-        },
+        query: data, // if there is a query in the queries, not the default case
         tags: {
           tags_grid: this_is_a_tag_search ? true : false,
           tag: tag,
           global_tags: global_tags,
         },
+        ...pageContext,
       }}
     />
   )
