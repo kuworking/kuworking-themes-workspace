b27a2706f7d299e1f7b960559cd97ad751aa3aee
kuworking <kuworking@gmail.com>
2020-08-19 20:55:29 +0200

    add global styles from emotion

--- blocks/block-masonry/src/components/structure.js
+++ blocks/block-masonry/src/components/structure.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import { GlobalStyles } from './globalstyles'
+import { GlobalStyles } from '@kuworking/methods'
 import { Grid } from './grid'
 
 export const Structure = ({ folder = '/', items = [] }) => {
