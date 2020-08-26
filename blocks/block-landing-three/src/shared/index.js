da07e8fbe34a705b04c2707e7db99df5d26fdfc1
kuworking <kuworking@gmail.com>
2020-08-26 23:32:11 +0200

    unneded share

--- blocks/block-landing-three/src/shared/index.js
+++
@@ -1,7 +0,0 @@
-/* version 2020.08.26 */
-
-export { SocialShare } from './socialshare'
-export { SPACE } from './space'
-export { Spinner01 } from './spinner'
-export { BImg, Img, KwImg } from './image'
-export { BImageSet } from './image-set'
